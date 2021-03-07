b092baea6334fdc9182d31db00a298f0daa3b1c6
Ralf Lopez <ralflopez3@gmail.com>
2021-03-07 23:37:04 +0800

    inital commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as colors from './styles/colors';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.BG,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ styles/colors.ts
@@ -0,0 +1,7 @@
+export const PRIMARY: string = '#7289DA';
+export const SECONADRY: string = '#DA7E72';
+export const DARK: string = '#23272A';
+export const DARK_LIGHTER: string = '#2C2F33';
+export const BG: string = DARK;
+export const BG_LIGHTER: string = DARK_LIGHTER;
+export const WHITE: string = '#FFFFFF';
